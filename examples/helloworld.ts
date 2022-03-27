d9074056e0776a595399efd73256b99b67cba551
loading <49134864+load1n9@users.noreply.github.com>
2022-03-27 12:47:46 -0400

    fix: examples with temp typescript system

--- examples/helloworld.ts
+++ examples/helloworld.ts
@@ -1,12 +1,33 @@
-import { Whistle, load } from "../mod.ts";
-
-const bits =  await new Whistle(`
-// optional main function runs by default
-export fn main(): i32 {
-    // prints Hello World to the console 
-    printString("Hello World!")
+import { compile, lex, load, parse } from "../mod.ts";
+import { System } from "./utils.ts";
+const code = `
+export fn helloWorld(): i32 {
+    printString("Hello, world!")
     return 0
-}
-`).compile();
+}`;
 
-await load(bits);
+// Load the wasm module
+await load();
+// Lex the code
+const tokens = lex(code);
+console.log(tokens);
+// Parse the code
+const ast = parse(code);
+console.log(ast);
+// Compile the code
+const wasm = compile(code);
+const module = await WebAssembly.compile(wasm);
+
+const imports = {
+  sys: {
+    printInt: System.printInt,
+    printString: System.printString,
+  },
+};
+// Instantiate the module
+const instance = await WebAssembly.instantiate(module, imports);
+System.memory = (instance.exports.memory as WebAssembly.Memory) || undefined;
+// Get the exported function
+const { helloWorld } = instance.exports as { helloWorld: () => number };
+// Call the exported function which prints "Hello, world!"
+helloWorld();
